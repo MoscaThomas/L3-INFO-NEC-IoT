dac964f14313cb6b910146a4118f8be519a4a970
Abadie Thomas <abd.thom@gmail.com>
2023-02-24 16:05:07 +0100

    site

--- SiteIOT/data.php
+++ SiteIOT/data.php
@@ -1,11 +1,11 @@
 <?php
 // Informations d'identification pour la base de données
-$hostname = "mysql";  // Nom d'hôte du serveur MySQL
+$hostname = "localhost";  // Nom d'hôte du serveur MySQL
 $username = "root";  // Nom d'utilisateur MySQL
 $password = "root";  // Mot de passe
 
 // Nom de la base de données à laquelle se connecter
-$database = "IOT";
+$database = "money";
 
 // Connexion à la base de données avec l'interface PDO
 $pdo = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
